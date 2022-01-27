55d69a51d0eaffdc50d754e8d7fb29e27b71582f
dileep-manchikanti <60563647+dileep-manchikanti@users.noreply.github.com>
2022-01-27 15:01:21 +0530

    Update script.js

--- solutions/assignment5-solution-starter/js/script.js
+++ solutions/assignment5-solution-starter/js/script.js
@@ -62,7 +62,7 @@ var switchMenuToActive = function () {
 
 // On page load (before images or CSS)
 document.addEventListener("DOMContentLoaded", function (event) {
-
+console.log("hi1");
 // TODO: STEP 0: Look over the code from
 // *** start ***
 // to
@@ -92,7 +92,7 @@ $ajaxUtils.sendGetRequest(
 // Builds HTML for the home page based on categories array
 // returned from the server.
 function buildAndShowHomeHTML (categories) {
-
+console.log("hi2");
   // Load home snippet page
   $ajaxUtils.sendGetRequest(
     homeHtmlUrl,
@@ -102,7 +102,7 @@ function buildAndShowHomeHTML (categories) {
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
       var chosenCategoryShortName = chooseRandomCategory (categories);
-      console.log("hi");
+      console.log("hi4");
       console.log(chosenCategoryShortName);
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
@@ -133,6 +133,7 @@ function buildAndShowHomeHTML (categories) {
 // Given array of category objects, returns a random category object.
 function chooseRandomCategory (categories) {
   // Choose a random index into the array (from 0 inclusively until array length (exclusively))
+  console.log("hi3");
   var randomArrayIndex = Math.floor(Math.random() * categories.length);
 
   // return category object with that randomArrayIndex
